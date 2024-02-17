fc6d2897bfd5c85d15b136823f22fbc3a7fbcb1a
Lucius <LaMiller0000@gmail.com>
2024-02-17 14:51:27 -0500

    AddedBrush

--- PaintProgram/Form1.Designer.cs
+++ PaintProgram/Form1.Designer.cs
@@ -29,14 +29,28 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
+        panel1 = new Panel();
         SuspendLayout();
+        // 
+        // panel1
+        // 
+        panel1.BackColor = Color.FromArgb(255, 128, 128);
+        panel1.Location = new Point(3, 12);
+        panel1.Name = "panel1";
+        panel1.Size = new Size(1374, 584);
+        panel1.TabIndex = 0;
+        panel1.MouseDown += panel1_MouseDown;
+        panel1.MouseMove += panel1_MouseMove;
+        panel1.MouseUp += panel1_MouseUp;
         // 
         // Form1
         // 
-        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleDimensions = new SizeF(12F, 30F);
         AutoScaleMode = AutoScaleMode.Font;
         BackColor = Color.FromArgb(50, 50, 50);
-        ClientSize = new Size(1089, 566);
+        ClientSize = new Size(1867, 1132);
+        Controls.Add(panel1);
+        Margin = new Padding(5, 6, 5, 6);
         Name = "Form1";
         Text = "Form1";
         Click += Form1_Click;
@@ -50,4 +64,5 @@ partial class Form1
     private EllipseShape ellipseShape1;
     private CrossShape crossShape1;
     private Label label1;
+    private Panel panel1;
 }
--- PaintProgram/Form1.cs
+++ PaintProgram/Form1.cs
@@ -22,10 +22,13 @@ public partial class Form1 : Form
         CreateShape<Star5Points>();
         CreateShape<Star6Points>();
         CreateShape<TrapazoidShape>();
+        Cursor.Hide();
 
         InitializeCustomTitleBar();
+        resizerF();
+
     }
-    
+
     private void CreateShape<T>() where T : Shape, new()
     {
         var shape = new T { Owner = this };
@@ -42,4 +45,115 @@ public partial class Form1 : Form
         ControlBox = false;
     }
     private void Form1_Click(object sender, EventArgs e) => shapes.ForEach(s => s.HideHandles());
+
+    enum EPaintTool { None, Brush, Spray, Fountain, Eraser }
+
+    public int AbsoluteRadius = 20;
+    public bool mouseIsDown = false;
+
+    private EPaintTool paintTool = EPaintTool.Brush;
+    private Graphics g;
+    private Color absoluteColor = Color.Black;
+    private int x = -1;
+    private int y = -1;
+    private Brush brush;
+    private Pen pen;
+
+    public void resizerF()
+    {
+        g = panel1.CreateGraphics();
+        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+        brush = new SolidBrush(absoluteColor);
+        pen = new Pen(absoluteColor, 3);
+        pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+    }
+
+
+
+    private (int, int, int) randomPoint(int x, int y, int radius)
+    {
+        Random random = new();
+        x = random.Next(x - radius, x + radius);
+        y = random.Next(y - radius, y + radius);
+        return (x, y, 0);
+    }
+
+    private int DetermineFountainThickness(int x3, int y3)
+    {
+        double x1 = x3; double y1 = y3;
+        double x2 = x; double y2 = y;
+        x2 -= x1; y2 -= y1;
+        x2 *= x2; y2 *= y2;
+        double sX = Math.Sqrt(x2);
+        double sY = Math.Sqrt(y2);
+
+        return sX < sY ? 10 : 2;
+    }
+
+    //the function which draws onto the panel
+    private int PaintScreen(int x, int y)
+    {
+        switch (paintTool)
+        {
+            case EPaintTool.Brush:
+                g.DrawLine(pen, new PointF(this.x, this.y), new PointF(x, y));
+                break;
+            case EPaintTool.Spray:
+                for (int i = 0; i < 100; i++)
+                {
+                    var imposter = randomPoint(x, y, AbsoluteRadius);
+                    g.FillEllipse(brush, imposter.Item1, imposter.Item2, 2, 2);
+                }
+                break;
+            case EPaintTool.Fountain:
+                int width = DetermineFountainThickness(x, y);
+                g.FillRectangle(brush, x, y, width, 3);
+                break;
+            case EPaintTool.Eraser:
+
+                brush = new SolidBrush(panel1.BackColor);
+                g.FillEllipse(brush, x, y, AbsoluteRadius, AbsoluteRadius);
+                break;
+            default:
+                MessageBox.Show("def");
+                break;
+
+        }
+        return 0;
+    }
+
+    private void panel1_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (mouseIsDown && x != -1 && y != -1)
+        {
+            PaintScreen(e.X, e.Y);
+            (x, y) = (e.X, e.Y);
+        }
+    }
+
+    private void panel1_MouseDown(object sender, MouseEventArgs e)
+    {
+        (x, y) = (e.X, e.Y);
+        PaintScreen(e.X, e.Y);
+        mouseIsDown = true;
+    }
+
+    private void panel1_MouseUp(object sender, MouseEventArgs e)
+    {
+        mouseIsDown = false;
+    }
+
+    //this lets the expanded canvas be useable.
+    private void panel1_Resize(object sender, EventArgs e)
+    {
+        //InitializeComponent();
+        resizerF();
+    }
+
+    //erases the canvas
+    private void eraser()
+    {
+        panel1.Invalidate();
+    }
 }
+
