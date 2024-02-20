e35ef885fdc0febc77f8a72c48e25fed0b7128ca
Austin Bryan <w.austin.bryan@gmail.com>
2024-02-20 05:08:00 -0500

    Made fully functional color wheel.

---
+++ PaintProgram/Stupid.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace PaintProgram
+{
+    partial class Stupid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            colorWheelPictureBox = new PictureBox();
+            valueSliderPictureBox = new PictureBox();
+            colorPreviewPictureBox = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)colorWheelPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)valueSliderPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)colorPreviewPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // colorWheelPictureBox
+            // 
+            colorWheelPictureBox.BackColor = Color.Transparent;
+            colorWheelPictureBox.Location = new Point(12, 12);
+            colorWheelPictureBox.Name = "colorWheelPictureBox";
+            colorWheelPictureBox.Size = new Size(200, 200);
+            colorWheelPictureBox.TabIndex = 2;
+            colorWheelPictureBox.TabStop = false;
+            colorWheelPictureBox.Paint += colorWheelPictureBox_Paint;
+            colorWheelPictureBox.MouseDown += colorWheelPictureBox_MouseDown;
+            colorWheelPictureBox.MouseEnter += colorWheelPictureBox_MouseEnter;
+            colorWheelPictureBox.MouseLeave += colorWheelPictureBox_MouseLeave;
+            colorWheelPictureBox.MouseMove += colorWheelPictureBox_MouseMove;
+            colorWheelPictureBox.MouseUp += colorWheelPictureBox_MouseUp;
+            // 
+            // valueSliderPictureBox
+            // 
+            valueSliderPictureBox.BackColor = Color.Transparent;
+            valueSliderPictureBox.Location = new Point(218, 12);
+            valueSliderPictureBox.Name = "valueSliderPictureBox";
+            valueSliderPictureBox.Size = new Size(15, 200);
+            valueSliderPictureBox.TabIndex = 3;
+            valueSliderPictureBox.TabStop = false;
+            valueSliderPictureBox.Paint += valueSliderPictureBox_Paint;
+            valueSliderPictureBox.MouseDown += valueSliderPictureBox_MouseDown;
+            valueSliderPictureBox.MouseEnter += valueSliderPictureBox_MouseEnter;
+            valueSliderPictureBox.MouseLeave += valueSliderPictureBox_MouseLeave;
+            valueSliderPictureBox.MouseMove += valueSliderPictureBox_MouseMove;
+            valueSliderPictureBox.MouseUp += valueSliderPictureBox_MouseUp;
+            // 
+            // colorPreviewPictureBox
+            // 
+            colorPreviewPictureBox.BackColor = Color.Red;
+            colorPreviewPictureBox.Location = new Point(239, 12);
+            colorPreviewPictureBox.Name = "colorPreviewPictureBox";
+            colorPreviewPictureBox.Size = new Size(50, 50);
+            colorPreviewPictureBox.TabIndex = 4;
+            colorPreviewPictureBox.TabStop = false;
+            colorPreviewPictureBox.Paint += colorPreviewPictureBox_Paint;
+            // 
+            // Stupid
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(colorPreviewPictureBox);
+            Controls.Add(valueSliderPictureBox);
+            Controls.Add(colorWheelPictureBox);
+            Name = "Stupid";
+            Text = "Stupid";
+            ((System.ComponentModel.ISupportInitialize)colorWheelPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)valueSliderPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)colorPreviewPictureBox).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private PictureBox colorWheelPictureBox;
+        private PictureBox valueSliderPictureBox;
+        private PictureBox colorPreviewPictureBox;
+    }
+}
---
+++ PaintProgram/Stupid.cs
@@ -0,0 +1,211 @@
+﻿using System.Linq.Expressions;
+using System.Windows.Forms;
+
+namespace PaintProgram;
+
+public partial class Stupid : Form
+{
+    private Point wheelCursorPoint, valueCursorPoint;
+    private bool sliderMouseDown;
+    private float sliderValue = 1.0f;
+    private bool wheelMouseDown;
+    private const int cursorDiameter = 12;
+    private readonly int sliderMin, sliderMax;
+    private readonly Dictionary<float, Bitmap> cachedBitmaps = new();
+
+    public Stupid()
+    {
+        InitializeComponent();
+
+        BackColor = Color.LimeGreen;
+        TransparencyKey = BackColor;
+        ShowInTaskbar = false;
+        FormBorderStyle = FormBorderStyle.None;
+
+        valueSliderPictureBox.Image = GenerateValueSlider();
+
+        wheelCursorPoint = new Point(colorWheelPictureBox.Width / 2 - cursorDiameter / 2,
+                                     colorWheelPictureBox.Height / 2 - cursorDiameter / 2);
+        valueCursorPoint = new Point(1, 1);
+        sliderMin = 1;
+        sliderMax = valueSliderPictureBox.Height - 1 - cursorDiameter;
+
+        colorWheelPictureBox.Refresh();
+        valueSliderPictureBox.Refresh();
+
+        // Generate the bitmaps for the color wheel for every possible value.
+        // This dramatically improves preformance of the color wheel slider
+        for (int i = 0; i < 255; i++)
+        {
+            sliderValue = (float)i / 255;
+            colorWheelPictureBox.Image = GenerateColorWheel();
+        }
+    }
+
+    private Bitmap GenerateValueSlider()
+    {
+        int length = valueSliderPictureBox.Height;
+        int width = valueSliderPictureBox.Width;
+
+        Bitmap valueSlider = new(width, length);
+
+        for (int y = 0; y < length; y++)
+        {
+            double value = (double)y / (length - 1); // Calculate value from top (1.0) to bottom (0.0)
+
+            Color color = Color.FromArgb((int)(255 * value), (int)(255 * value), (int)(255 * value));
+
+            for (int x = 0; x < width; x++)
+                valueSlider.SetPixel(x, length - 1 - y, color); // Set pixel from bottom to top
+        }
+
+        return valueSlider;
+    }
+    private Bitmap GenerateColorWheel()
+    {
+        if (cachedBitmaps.ContainsKey(sliderValue))
+            return cachedBitmaps[sliderValue];
+
+        int radius = colorWheelPictureBox.Height / 2;
+        Bitmap colorWheel = new(radius * 2, radius * 2);
+
+        int centerX = radius;
+        int centerY = radius;
+
+        for (int y = -radius; y < radius; y++)
+            for (int x = -radius; x < radius; x++)
+            {
+                double distance = Math.Sqrt(x * x + y * y);
+
+                if (distance > radius)
+                    continue;
+                double hue = Math.Atan2(y, x) / (2 * Math.PI) + 0.5; // Normalize to range [0, 1]
+                double saturation = distance / radius;               // Increase saturation as distance from center increases
+
+                Color color = ColorFromHSV(hue, saturation, sliderValue);
+                colorWheel.SetPixel(centerX + x, centerY + y, color);
+            }
+
+        cachedBitmaps.Add(sliderValue, colorWheel);
+
+        return colorWheel;
+    }
+    private static Color ColorFromHSV(double hue, double saturation, double value)
+    {
+        int hi = Convert.ToInt32(Math.Floor(hue * 6)) % 6;
+        double f = hue * 6 - Math.Floor(hue * 6);
+
+        value *= 255;
+        int v = Convert.ToInt32(value);
+        int p = Convert.ToInt32(value * (1 - saturation));
+        int q = Convert.ToInt32(value * (1 - f * saturation));
+        int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+        if (hi == 0)
+            return Color.FromArgb(255, v, t, p);
+        else if (hi == 1)
+            return Color.FromArgb(255, q, v, p);
+        else if (hi == 2)
+            return Color.FromArgb(255, p, v, t);
+        else if (hi == 3)
+            return Color.FromArgb(255, p, q, v);
+        else if (hi == 4)
+            return Color.FromArgb(255, t, p, v);
+        else return Color.FromArgb(255, v, p, q);
+    }
+
+    private void colorWheelPictureBox_Paint(object sender, PaintEventArgs e) => DrawCursor(e, Brushes.Black, wheelCursorPoint);
+    private void valueSliderPictureBox_Paint(object sender, PaintEventArgs e) => DrawCursor(e, Brushes.Black, valueCursorPoint);
+
+    private void DrawCursor(PaintEventArgs e, Brush brush, Point point)
+    {
+        int offset = 2;
+        e.Graphics.DrawEllipse(new Pen(Brushes.White, 1.6f), x: point.X, point.Y, cursorDiameter, cursorDiameter);
+        e.Graphics.DrawEllipse(new Pen(Brushes.Black, 1.6f), x: point.X + offset / 2, point.Y + offset / 2, cursorDiameter - offset, cursorDiameter - offset);
+    }
+
+    private void colorWheelPictureBox_MouseEnter(object sender, EventArgs e) => Cursor = Cursors.Cross;
+    private void colorWheelPictureBox_MouseLeave(object sender, EventArgs e) => Cursor = Cursors.Default;
+    private void valueSliderPictureBox_MouseEnter(object sender, EventArgs e) => Cursor = Cursors.Cross;
+    private void valueSliderPictureBox_MouseLeave(object sender, EventArgs e) => Cursor = Cursors.Default;
+    private void valueSliderPictureBox_MouseUp(object sender, MouseEventArgs e) => sliderMouseDown = false;
+    private void valueSliderPictureBox_MouseDown(object sender, MouseEventArgs e) => UpdateSliderCursor(e);
+
+    private void UpdateSliderCursor(MouseEventArgs e)
+    {
+        sliderMouseDown = true;
+        valueCursorPoint = new Point(valueCursorPoint.X, Math.Clamp(e.Y, sliderMin, sliderMax));
+
+        sliderValue = (1 - valueCursorPoint.Y / (float)(sliderMax));
+        sliderValue = ClampToNearestMultiple(sliderValue);
+        valueSliderPictureBox.Refresh();
+        colorWheelPictureBox.Image = GenerateColorWheel();
+        SelectColor();
+    }
+
+    private static float ClampToNearestMultiple(float value)
+    {
+        float scaledValue = value * 255;                            // Scale up to range [0, 255]
+        float roundedValue = (float)Math.Round((double)scaledValue); // Round to nearest integer
+        float clampedValue = roundedValue / 255;                     // Scale back down to range [0, 1]
+
+        return clampedValue;
+    }
+
+    private void valueSliderPictureBox_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (!sliderMouseDown)
+            return;
+        UpdateSliderCursor(e);
+    }
+
+    private void colorWheelPictureBox_MouseDown(object sender, MouseEventArgs e)
+    {
+        wheelMouseDown = true;
+        SelectColor();
+        UpdateWheelCursor(e);
+    }
+
+    private void UpdateWheelCursor(MouseEventArgs e)
+    {
+        wheelCursorPoint = new Point (e.Location.X - cursorDiameter / 2, e.Location.Y - cursorDiameter / 2);
+        colorWheelPictureBox.Refresh();
+    }
+    private void SelectColor()
+    {
+        const int cursorOffset = 17;
+        var colorWheelBitmap = cachedBitmaps[sliderValue];
+        Point adjustedPoint  = new (wheelCursorPoint.X + Location.X + cursorOffset, wheelCursorPoint.Y + Location.Y + cursorOffset);
+        Point cursorPosition = colorWheelPictureBox.PointToClient(adjustedPoint);
+
+        if (cursorPosition.X >= 0 && cursorPosition.X < colorWheelPictureBox.Width &&
+            cursorPosition.Y >= 0 && cursorPosition.Y < colorWheelPictureBox.Height)
+        {
+            Color currentColor = colorWheelBitmap.GetPixel(cursorPosition.X, cursorPosition.Y);
+            colorPreviewPictureBox.BackColor = currentColor;
+        }
+    }
+
+    private void colorWheelPictureBox_MouseUp(object sender, MouseEventArgs e)
+    {
+        wheelMouseDown = false;
+    }
+
+    private void colorWheelPictureBox_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (!wheelMouseDown)
+            return;
+
+        SelectColor();
+        UpdateWheelCursor(e);
+    }
+
+    private void colorPreviewPictureBox_Paint(object sender, PaintEventArgs e)
+    {
+        int width = colorPreviewPictureBox.Width;
+        int height = colorPreviewPictureBox.Height;
+
+        e.Graphics.DrawRectangle(new Pen(Brushes.White, 8), 0, 0, width, height);
+        e.Graphics.DrawRectangle(new Pen(Brushes.Black, 4), 0, 0, width, height);
+    }
+}
