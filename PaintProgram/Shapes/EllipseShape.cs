8c2874626518c808069de18985cbc1d97ba2c4e6
Austin Bryan <w.austin.bryan@gmail.com>
2024-02-21 01:23:57 -0500

    Fixed ellipse color

--- PaintProgram/Shapes/EllipseShape.cs
+++ PaintProgram/Shapes/EllipseShape.cs
@@ -6,6 +6,9 @@ public partial class EllipseShape : RectangleShape
 
     protected override void DrawShape(PaintEventArgs e)
     {
-        e.Graphics.FillEllipse(Brushes.MediumBlue, new Rectangle(new Point(Gap, Gap), new Size(Width - Gap * 2, Height - Gap * 2)));
+        Rectangle rect = new (new Point(Gap, Gap), new Size(Width - Gap * 2, Height - Gap * 2));
+
+        e.Graphics.FillEllipse(new SolidBrush(ShapeColor), rect);
+        e.Graphics.DrawEllipse(new Pen(new SolidBrush(BorderColor), BorderThickness), rect);
     }
 }
