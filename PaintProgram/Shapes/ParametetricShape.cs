2950ae8c509b4fb1a714b278e2985c6886dfcf6b
Austin Bryan <w.austin.bryan@gmail.com>
2024-02-22 00:19:48 -0500

    Replaced first few items

--- PaintProgram/Shapes/ParametetricShape.cs
+++ PaintProgram/Shapes/ParametetricShape.cs
@@ -9,44 +9,32 @@ public partial class ParametricShape : Shape
     public float Alpha { get; set; } = 0.25f;
     protected virtual float WidthAdjustment { get; }
     protected virtual int AlphaPointIndex { get; } = 0;
-    private Rectangle[] alphaHandles;
+    private Rectangle[] alphaRects;
+    private readonly AlphaHandle[] alphaHandles;
 
-    public ParametricShape() => InitializeComponent();
-
-    protected void DrawAlphaHandle(PaintEventArgs e, int pointIndex)
+    public ParametricShape()
     {
-        if (!shouldShowHandles)
-            return;
-
-        const int diamondRadius = 5;
-        Point[] diamondPoints = new Point[]
-        {
-            points[pointIndex].Add(new(0, -diamondRadius)),
-            points[pointIndex].Add(new(diamondRadius, 0)),
-            points[pointIndex].Add(new(0, diamondRadius)),
-            points[pointIndex].Add(new(-diamondRadius, 0))
-        };
-        e.Graphics.DrawPolygon(new Pen(Color.Black, 2), diamondPoints);
-        e.Graphics.FillPolygon(Brushes.Orange, diamondPoints);
-
-        alphaHandles[0] = new Rectangle(points[pointIndex].X - diamondRadius, points[pointIndex].Y - diamondRadius, 10, 10);
+        InitializeComponent();
+        alphaHandles = new AlphaHandle[1];
+        alphaHandles[0] = new AlphaHandle(this);
     }
-    protected virtual float GetAlpha(MouseEventArgs e) => 1 - (e.X - (Width / 2)) / (float)Width * WidthAdjustment;
 
+    protected virtual float GetAlpha(MouseEventArgs e) => 1 - (e.X - (Width / 2)) / (float)Width * WidthAdjustment;
+    
     protected override void InitializeHandles()
     {
         base.InitializeHandles();
-        alphaHandles = new Rectangle[1];
+        alphaRects = new Rectangle[1];
     }
     protected override void OnPaint(PaintEventArgs e)
     {
         Alpha = Math.Max(Alpha, MinAlpha);
         base.OnPaint(e);
-        DrawAlphaHandle(e, AlphaPointIndex);
+        alphaHandles.ToList().ForEach(a => a.Draw(e));
     }
     protected override void UpdateCursor(MouseEventArgs e)
     {
-        if (alphaHandles.Length > 0 && alphaHandles[0].Contains(e.Location))
+        if (alphaRects.Length > 0 && alphaRects[0].Contains(e.Location))
             Cursor = Cursors.Cross;
         else base.UpdateCursor(e);
     }
@@ -57,14 +45,15 @@ public partial class ParametricShape : Shape
     }
     protected override void OnMouseDown(MouseEventArgs e)
     {
-        if (alphaHandles.Length > 0 && alphaHandles[0].Contains(e.Location))
-        {
-            State = State.ChangingAlpha;
-            moveStart = e.Location;
-        }
-        else base.OnMouseDown(e);
+        foreach (var alphaHandle in alphaHandles)
+            if (alphaHandle.IsPressed(e))
+            {
+                (State, moveStart) = (State.ChangingAlpha, e.Location);
+                return;
+            }
+        base.OnMouseDown(e);
     }
 
     protected int ShortLength(int n) => (int)(n * Alpha);
     protected int LongLength(int n)  => (int)(n * (1 - Alpha));
-}
+}//70
