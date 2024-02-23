9bf23e927723585f6f1da305d7375392eb9fc2c4
Austin Bryan <w.austin.bryan@gmail.com>
2024-02-23 01:31:58 -0500

    Added pixeltextbox and alpha handler

---
+++ PaintProgram/PixelTextBox.cs
@@ -0,0 +1,19 @@
+﻿using static PaintProgram.Shapes.ParametricShape;
+
+namespace PaintProgram;
+
+public partial class PixelTextBox : UserControl
+{
+    public string TextBoxText { get; set; }
+    public string LabelText { get; set; }
+    public bool AllowDecimals { get; set; }
+
+    public delegate void InputSubmitHandler(double parsedTexxt);
+    public event InputSubmitHandler InputSubmit;
+
+    public PixelTextBox() => InitializeComponent();
+
+    public void UpdateAlphaBounds(AlphaHandle alphaHandle) 
+    {
+    }
+}
---
+++ PaintProgram/Shapes/AlphaHandle.cs
@@ -0,0 +1,44 @@
+﻿using static PaintProgram.Shapes.ParametricShape;
+
+namespace PaintProgram.Shapes;
+
+using GetAlphaFunc = Func<MouseEventArgs, AlphaHandle, float>;
+
+public partial class ParametricShape
+{
+    public class AlphaHandle
+    {
+        public float Alpha    { get; set; }
+        public float MinAlpha { get; private set; }
+        public float MaxAlpha { get; set;}
+        public bool IsPressed { get; set; }
+        private ParametricShape Owner;
+        private readonly int pointIndex;
+        private Point[] Points => Owner.points;
+        private Rectangle rect;
+        GetAlphaFunc GetAlpha;
+
+        public AlphaHandle(ParametricShape parameterticShape, int alphaPointIndex, float alpha, float minAlpha, float maxAlpha, GetAlphaFunc getAlpha) => (Owner, pointIndex, Alpha, MinAlpha, MaxAlpha, GetAlpha) = (parameterticShape, alphaPointIndex, alpha, minAlpha, maxAlpha, getAlpha);
+
+        public void Draw(PaintEventArgs e)
+        {
+            //if (!shouldShowHandles)
+            //    return;
+
+            const int diamondRadius = 5;
+            Point[] diamondPoints = new Point[]
+            {
+                Points[pointIndex].Add(new(0, -diamondRadius)),
+                Points[pointIndex].Add(new(diamondRadius, 0)),
+                Points[pointIndex].Add(new(0, diamondRadius)),
+                Points[pointIndex].Add(new(-diamondRadius, 0))
+            };
+            e.Graphics.DrawPolygon(new Pen(Color.Black, 2), diamondPoints);
+            e.Graphics.FillPolygon(Brushes.Orange, diamondPoints);
+
+            rect = new Rectangle(Points[pointIndex].X - diamondRadius, Points[pointIndex].Y - diamondRadius, 10, 10);
+        }
+        public bool IsHovered(MouseEventArgs e) => rect.Contains(e.Location);
+        public void AdjustAlpha(MouseEventArgs e) => Alpha = Math.Clamp(GetAlpha(e, this), MinAlpha, MaxAlpha);
+    }
+}
