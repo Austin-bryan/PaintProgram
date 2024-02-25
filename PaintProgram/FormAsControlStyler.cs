e8d8b6bd7237fdf2ac84e29839192ff1e918b606
Austin Bryan <w.austin.bryan@gmail.com>
2024-02-24 23:13:42 -0500

    Renamed FormHider

---
+++ PaintProgram/FormAsControlStyler.cs
@@ -0,0 +1,17 @@
+﻿/* Project: Paint
+ * Authors: Austin Bryan, Lucius Miller, Noah Curtis
+ * Class: Foundations in App Development
+ * Date: February 24th, 2024*/
+
+namespace PaintProgram;
+
+public static class FormAsControlStyler
+{
+    public static void StyleFormAsControl(Form form)
+    {
+        form.BackColor       = Color.LimeGreen;
+        form.TransparencyKey = form.BackColor;
+        form.ShowInTaskbar   = false;
+        form.FormBorderStyle = FormBorderStyle.None;
+    }
+}
